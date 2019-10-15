add9f01a97cfb6e5d13d4a3abafcb156a3b2807e
amanvir uppal <amanvir.uppal@icloud.com>
2019-10-15 07:00:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,4 +1,9 @@
-def square_array(array)
- numbers = [1, 2, 3]
- 
+def square_array(numbers)
+  new_array = []
+  counter = 0 
+  while counter < numbers.length()
+  new_array.push(numbers[counter] * numbers[counter])
+  counter += 1
+  end 
+  return new_array
 end
